017130a46fb7599196af0fe6c0ed5ab98f899621
gmmmmd <zoopark86@yandex.ru>
2023-01-06 19:08:40 +0400

    feat: add complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let number = n;
+  if (n < 0) {
+    number = number * -1;
+  }
+  const result = number.toString().split('').reverse().join('');
+  return result;
 }
